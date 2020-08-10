6b9a951fdbeb419e1313dd0bbee37f6090b5657e
Satyajit Sarangi <satyajit@zerodha.com>
2020-08-10 11:15:21 +0530

    Added support for int, bigint and text array dataypes for Postgres. _TYPE in Postgres represent array types. eg. _INT4, _INT8, _TEXT

--- backends/sqldb.go
+++ backends/sqldb.go
@@ -259,6 +259,13 @@ func (s *sqlDB) createTableSchema(cols []string, colTypes []*sql.ColumnType) ins
 			if s.dbType != dbTypePostgres {
 				typ = "TEXT"
 			}
+		// _INT4, _INT8, _TEXT represent array types in Postgres
+		case "_INT4": // Postgres
+			typ = "_INT4"
+		case "_INT8": // Postgres
+			typ = "_INT8"
+		case "_TEXT": // Postgres
+			typ = "_TEXT"
 		default:
 			typ = "TEXT"
 		}
